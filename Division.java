a330360144ade2a2b42841cbd482521a5636dc94
snegastud <129963809+snegastud@users.noreply.github.com>
2025-01-17 09:17:50 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Division{
+public static void main(String[] args){
+Scanner snega=new Scanner(System.in);
+int a=snega.nextInt();
+if(a%4==0){
+System.out.println("divisible by four");
+}
+else{
+System.out.println("Not divisible by four");
+}
+}
+}

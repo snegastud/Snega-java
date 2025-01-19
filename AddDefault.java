e27c0ce8519f14f7c38b369971c9edc4a8ebd0dc
snegastud <129963809+snegastud@users.noreply.github.com>
2025-01-19 20:59:48 +0530

    Add files via upload

---
+++ AddDefault.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class AddDefault{
+int num1;
+int num2;
+AddDefault(){
+num1=10;
+num2=20;
+System.out.println("add two numbers:"+(num1+num2));
+}
+public static void main(String[] args){
+AddDefault add=new AddDefault();
+
+}
+}

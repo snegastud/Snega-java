cccb98962d6b379268ba26826a67d484bf60b61c
snegastud <129963809+snegastud@users.noreply.github.com>
2025-01-21 23:17:33 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Reverse{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+System.out.println("enter the number:");
+int num=scan.nextInt();
+for(int i=20;i>=num;i--)
+{
+System.out.println("reverse program 20 to 10:"+i);
+}
+}
+}

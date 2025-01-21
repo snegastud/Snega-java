36af5a03d68590c7aeddb7891bf85694843f1fb3
snegastud <129963809+snegastud@users.noreply.github.com>
2025-01-21 23:17:01 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Numbers{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the numbers:")
+int num=scan.nextInt();
+
+for(int i=10;i<=num;i=i+10){
+System.out.println(i);
+}
+
+}
+}

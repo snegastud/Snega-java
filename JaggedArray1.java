08bae02918bbdebf26fd0d97994ca6ca6fd6d615
snegastud <129963809+snegastud@users.noreply.github.com>
2025-02-11 17:52:26 +0530

    Add files via upload

---
+++ JaggedArray1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class JaggedArray1
+{
+public static void main(String[] args){
+int[][] arr=new int[3][];
+arr[0]=new int[1];
+arr[1]=new int[2];
+arr[2]=new int[3];
+int count=0;
+for(int i=0;i<arr.length;i++){
+for(int j=0;j<arr[i].length;j++){
+arr[i][j]=count++;
+}
+}
+for(int i=0;i<arr.length;i++){
+for(int j=0;j<arr[i].length;j++){
+System.out.print(arr[i][j]);
+}
+System.out.println();
+}
+}
+}

c25b4a3a73a47126239dfea353753e1a711f3af3
snegastud <129963809+snegastud@users.noreply.github.com>
2025-02-13 22:01:19 +0530

    Add files via upload

---
+++ ArrayListExample1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArrayListExample1{
+public static void main(String[] args){
+ArrayList<String> arr=new ArrayList<>();
+//add the array elements
+arr.add("red");
+arr.add("green");
+arr.add("blue");
+arr.add("yellow");
+arr.add("black");
+arr.add("orange");
+//display the elements
+System.out.println("enter the elements:"+arr);
+
+}
+}
